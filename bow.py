9d1b641fb4079998739745df24efa0827f259839
Josh Vahala <vahala@wisc.edu>
2017-02-19 19:22:38 -0600

    fit_transform method added to BOW(). Added option to return header and article body BOW results separately using the 'separate_vectors' bool option in transform() or fit_transform()

--- bow.py
+++ bow.py
@@ -21,7 +21,7 @@ class BOW:
         pass
 
     def fit(self,X,y=None,new_pickle=False):
-        # collect word dictionary (string keys with index values) from ./data/word-dict.pickle, or create it. If new_pickle is True, ./data/word-dict.pickle will be created or overwritten 
+        # collect word dictionary (string keys with index values) from ./data/word-dict.pickle, or create it. If new_pickle is True, ./data/word-dict.pickle will be created or overwritten.
         try:
             if not new_pickle:
                 self.words = pickle.load( open( "./data/word-dict.pickle", "rb" ))
@@ -42,26 +42,37 @@ class BOW:
 
         return self
 
-    def transform(self,X):
+    def transform(self,X,separate_vectors=False):
         '''
-        Transform list of tuples X = (headline, article) into Bag of Words matrix 
+        Transform list of tuples X = (headline, article) into Bag of Words matrix
 
         inputs:
-            - X = [(headline1,article1), (headline2,article2), ...] 
+            - X = [(headline1,article1), (headline2,article2), ...]
+            - separate_vectors (bool): if True, returns bowX = [headers_ndarray,articles_ndarray], else returns a single ndarray with combined header+article vectors per row
 
         outputs:
             - bowX = ndarray of shape (len(X),len(self.words)), each index of a row corresponds to the specific word in self.words and contains the number of instances of that word within the headline-article pair.
 
         '''
-        bowX = np.zeros((len(X),len(self.words)))
+        headerX = np.zeros((len(X),len(self.words)))
+        articleX = np.zeros_like(headerX)
 
         for i, x in enumerate(X):
             for word in x[0]:
-                bowX[i,:] += self.bowVector(word)
+                headerX[i,:] += self.bowVector(word)
             for word in x[1]:
-                bowX[i,:] += self.bowVector(word)
+                articleX[i,:] += self.bowVector(word)
 
-        return bowX
+        if separate_vectors:
+            return [headerX,articleX]
+        else:
+            return headerX+articleX
+
+    def fit_transform(self,X,y=None,new_pickle=False,separate_vectors=False):
+        self.fit(X,y,new_pickle)
+        output = self.transform(X,separate_vectors)
+        return output
+
 
     def bowVector(self,word):
         # defines a vector of length len(self.words) that is zero-valued everywhere but where the input word string is found
@@ -73,10 +84,32 @@ class BOW:
         return bow
 
 
-if __name__=='__main__':
+def test(n=10,use_fit_transform=True):
+    '''
+    Test BOW() object for n rows of the training dataset. 
+
+    inputs:
+        - n (int): number of rows of the training dataset to test BOW formulation on
+        - use_fit_transform (bool): if True, test is done using the BOW.fit_transform() method, else use BOW.fit() then BOW.transform() separately
+    '''
+
     import load
+
     X_train, y_train, X_test, y_test = load.load(True,False,False,False)
     bow = BOW()
-    bow.fit(X_train,new_pickle=False)
-    print(len(linear.words), 'unique words')
-    bow.transform(X_train[0:10])
+    if not use_fit_transform:
+        bow.fit(X_train,new_pickle=False)
+        output = bow.transform(X_train[0:n])
+        _using = 'fit() THEN tranform()'
+    else:
+        output = bow.fit_transform(X_train[0:n],new_pickle=False)
+        _using = 'fit_transform()'
+    print('Using {0}...\nWords found in each header/article\n'.format(_using),np.sum(output,axis=1))
+    output = bow.fit_transform(X_train[0:n],new_pickle=False,separate_vectors=True)
+    print('Words in each header, then words in each article\n{0}\n{1}'.format(np.sum(output[0],axis=1), np.sum(output[1], axis=1)))
+
+
+if __name__=='__main__':
+    n = 10
+    use_fit_transform = True
+    test(n,use_fit_transform)
